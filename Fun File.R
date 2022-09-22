63660fa3994854c653ef0c4cf475ba94bd7c6398
binf-singh <24smilelaughter@gmail.com>
2022-09-22 10:42:55 -0700

    Added comments and created scatter plot of vroom

--- Fun File.R
+++ Fun File.R
@@ -1,4 +1,11 @@
+# Download mtcars data
 data(mtcars)
+
+# Assign mtcars data to variable, vroom
 vroom <- mtcars
 
+# Check class of vroom
 class(vroom)
+
+# Plot mpg and hp as a scatter plot
+plot(vroom$hp, vroom$mpg, xlab = "HP", ylab = "MPG")
